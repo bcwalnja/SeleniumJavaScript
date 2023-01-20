dfb3a7ca00045f1687a8491b8b075d8fab68a1aa
Nathaniel Waldron <nathaniel@landistechnologies.com>
2023-01-20 12:20:14 -0500

    Added url for todo app

--- tests/FirstTest.js
+++ tests/FirstTest.js
@@ -1,5 +1,20 @@
 const {Builder} = require ("selenium-webdriver");
 
-async function example() {
-    var url = "https://lambdatest.github.io/docs/real-time-browser-testing";
+async function firstAutomationFunction() {
+    //var url = "https://lambdatest.github.io/docs/real-time-browser-testing";
+    var url = "https://todo-pwa.nico.dev/preact/"
+
+    // launch the browser
+    let driver = await new Builder().forBrowser("chrome").build();
+
+    // navigate to url
+    await driver.get(url);
+
+    // add a todo
+
+
+    // close the browser
 }
+
+// run the function
+firstAutomationFunction();
