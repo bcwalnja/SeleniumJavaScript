5e1012ba0051a136aa37ec5524fa87fef2c1e65c
Nathaniel Waldron <nathaniel@landistechnologies.com>
2023-01-20 16:20:44 -0500

    Basic functionality

--- tests/FirstTest.js
+++ tests/FirstTest.js
@@ -1,20 +1,29 @@
-const {Builder} = require ("selenium-webdriver");
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const assert = require ("assert")
+
 
 async function firstAutomationFunction() {
     //var url = "https://lambdatest.github.io/docs/real-time-browser-testing";
-    var url = "https://todo-pwa.nico.dev/preact/"
+    var url = "https://todo-pwa.nico.dev/preact/";
+    var searchId = "title";
+    let todoText = "Learn Selenium";
 
-    // launch the browser
-    let driver = await new Builder().forBrowser("chrome").build();
-
-    // navigate to url
+    let driver = await new Builder().forBrowser('chrome').build();
     await driver.get(url);
 
-    // add a todo
+    let search = await driver.findElement(By.id(searchId));
+    await search.sendKeys(todoText);
+    await search.sendKeys(Key.RETURN);
 
+    // assert
+    
 
-    // close the browser
+    await driver.quit();
 }
 
 // run the function
-firstAutomationFunction();
+for (let index = 0; index < 10; index++) {
+    firstAutomationFunction();
+}
+
+<ul class="w-11/12 max-w-lg mx-auto my-16 border-gray-300 border-b"><li class="flex w-full border-gray-300 border-t p-4 items-center text-sm hover:bg-white c-list-item"><input type="checkbox" class="mr-4 focus:shadow-outline focus:outline-none cursor-pointer " title="mark as done"><span class=" mr-4 inline-block">Task 3</span></li><li class="flex w-full border-gray-300 border-t p-4 items-center text-sm hover:bg-white c-list-item"><input type="checkbox" class="mr-4 focus:shadow-outline focus:outline-none cursor-pointer " title="mark as done"><span class=" mr-4 inline-block">Task 2</span></li><li class="flex w-full border-gray-300 border-t p-4 items-center text-sm hover:bg-white c-list-item"><input type="checkbox" class="mr-4 focus:shadow-outline focus:outline-none cursor-pointer " title="mark as done"><span class=" mr-4 inline-block">Task 1</span></li><li class="flex w-full border-gray-300 border-t p-4 items-center text-sm hover:bg-white c-list-item"><input type="checkbox" class="mr-4 focus:shadow-outline focus:outline-none cursor-pointer " title="mark as done"><span class=" mr-4 inline-block">Learn Selenium</span></li></ul>
